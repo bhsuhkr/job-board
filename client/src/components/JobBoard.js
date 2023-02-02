af8f1891bda550dc9ba6a6c515085cf0b9f2b3fb
Bohyun Suh <bhsuhkr@naver.com>
2023-02-01 22:38:12 -0600

    error handle

--- client/src/components/JobBoard.js
+++ client/src/components/JobBoard.js
@@ -4,12 +4,17 @@ import { useEffect, useState } from "react";
 
 function JobBoard() {
   const [jobs, setJobs] = useState([]);
+  const [error, setError] = useState(false);
   useEffect(() => {
     console.log("mounted"); // runs second, run only onces
-    getJobs().then(setJobs);
+    getJobs().then(setJobs).catch((err) => setError(true));
   }, []);
 
   console.log("[job]", jobs); //runs first, runs third: render again after getJobs() runs
+
+  if (error) {
+    return <p>Sorry, something went wrong.</p>
+  }
 
   return (
     <div>
