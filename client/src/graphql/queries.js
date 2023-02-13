9c87db639a15bc199f5d938ae37699ff89164f6a
Bohyun Suh <bhsuhkr@naver.com>
2023-02-12 21:25:40 -0600

    company hook

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -53,6 +53,20 @@ export const JOB_QUERY = gql`
   ${JOB_DETAIL_FRAGMENT}
 `;
 
+export const COMPANY_QUERY = gql`
+query ($id: ID!) {
+  company(id: $id) {
+    id
+    name
+    description
+    jobs {
+      id
+      title
+    }
+  }
+}
+`;
+
 export async function createJob(input) {
   const mutation = gql`
     mutation ($input: CreateJobInput!) {
@@ -81,25 +95,4 @@ export async function createJob(input) {
     },
   });
   return job;
-}
-
-export async function getCompany(id) {
-  const query = gql`
-    query ($id: ID!) {
-      company(id: $id) {
-        id
-        name
-        description
-        jobs {
-          id
-          title
-        }
-      }
-    }
-  `;
-  const variables = { id };
-  const {
-    data: { company },
-  } = await client.query({ query, variables });
-  return company;
 }
